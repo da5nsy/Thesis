5ad4ad27e8c446dfb28f31c75bb26fba6ea1b668
da5nsy <dannygarside@outlook.com>
2019-07-30 18:36:56 +0100

    changing the way I handle code chunks 2

--- CodeChunks/PTBColorimetryDemo_strippedDown.m
+++
@@ -1,8 +0,0 @@
-load spd_D65     % SPD: CIE D-series illuminant D65
-load sur_macbeth % SRF: macbeth colour checker dataset
-load T_xyz1931   % CMF: 1931 2deg 
-
-colourSignal = sur_macbeth.*spd_D65; %11 is 'lime green'
-XYZ = T_xyz1931*colourSignal;
-xy = [XYZ(1,:)./sum(XYZ);XYZ(2,:)./sum(XYZ)];
-
